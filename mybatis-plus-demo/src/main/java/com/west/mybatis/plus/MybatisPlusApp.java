e7f915fd9b13393952f6555c7f547ffef2eaf3f1
West121 <1976730661@qq.com>
2020-09-23 17:57:19 +0800

    mybatis plus init

---
+++ mybatis-plus-demo/src/main/java/com/west/mybatis/plus/MybatisPlusApp.java
@@ -0,0 +1,18 @@
+package com.west.mybatis.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author west
+ * @date 2020/9/23 17:50
+ */
+@SpringBootApplication
+public class MybatisPlusApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApp.class, args);
+    }
+}
---
+++ mybatis-plus-demo/src/main/java/com/west/mybatis/plus/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.west.mybatis.plus.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * mybatis-plus 配置
+ *
+ * @author west
+ * @date 2020/9/3 21:35
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.west.mybatis.plus.mapper.*")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
